4dd92a0e058e9d173c294eba312543c6a396177c
krynen <krynen2530@gmail.com>
2017-09-08 14:45:40 +0900

    0.2.3
    5 kinds of error are modified.

--- source/twip_apply.js
+++ source/twip_apply.js
@@ -1,18 +1,25 @@
 ﻿var twipMsg =
 	configData&&configData.twipCheersMsg?
 	configData.twipCheersMsg: 
-	configData.cheersMsg.replace("{bits}", "{wons}").replace(/\s*비트/, "원");
+	configData.cheersMsg;
 
 exports.apply = function(message, data) {
 	if (data.nick == "twipkr") {
 		if ( (message.indexOf("ACTION")!=-1) && (message.indexOf("\\u0001")!=-1) ) {
 			message = message.replace(/\\u0001/g, "").replace("ACTION", "");
 				
-			var amount = message.match(/ (\d+,)*\d{3}/g)[0].split(" ")[1].replace(/[^\d]/g, "");
-				
-			message = '<div class="chat_cheer_box">' +
-				twipMsg.replace("{wons}", amount) +
-				"</div>" + message;
+			var amount = message.match(/ (\d+,)*\d{3}/g);
+			if  (amount == null) {
+				twipMsg = twipMsg.replace("{bits}", "").
+					replace(/\{0:([^\}]*)}/g, "$1").replace(/\{!0:([^\}]*)}/g, "");
+			}
+			else {
+				amount = amount[0].split(" ")[1].replace(/[^\d]/g, "");
+				twipMsg = twipMsg.replace("{bits}", amount).replace(/((\d)\s+)?비트/, "$2원").
+					replace(/\{!0:([^\}]*)}/g, "$1").replace(/\{0:([^\}]*)}/g, "");
+			}
+			
+			message = '<div class="chat_cheer_box">' + twipMsg + "</div>" + message;
 		}
 	}
 	
