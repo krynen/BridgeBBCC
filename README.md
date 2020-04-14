9d7362f993946f852e0ba7b1236cdf1ff7b47da2
Mr.WaterT <krynen2530@gmail.com>
2020-04-14 19:14:41 +0900

    0.5.0
    skip 0.4.0
    renew package.json, readme.txt
    make all codes tap-to-space
    
    4 major changes
    4 error modifications
    3 remove deprecated
    3 minor changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Bridge BBCC
 ===
-version 0.3.0
+version 0.5.0
 
 
 
@@ -15,14 +15,12 @@ If you don't want to modify source files, just use released version.
 readme.txt 참조
 윈도우, XSplit 외의 환경에서 동작이 보증되지 않음
 
-크롬 등 브라우저를 통해 이용 시 클립 미리보기 등 일부 기능이 동작하지 않음
-
 
 ## License
 MIT
 
 ## Reference
 [JSAssist Open DCcon](https://github.com/rishubil/jsassist-open-dccon)
-By [rishubil]((https://github.com/rishubil), 2017, MIT License.
+By [rishubil](https://github.com/rishubil) MIT License.
 
 [Twitch Developer Documentation](https://dev.twitch.tv/docs)
--- source/twip_apply.js
+++ source/twip_apply.js
@@ -1,27 +1,26 @@
 ﻿var twipMsg =
-	configData&&configData.twipCheersMsg?
-	configData.twipCheersMsg: 
-	configData.cheersMsg;
+  configData&&configData.twipCheersMsg?
+  configData.twipCheersMsg:
+  configData.cheersMsg;
 
 exports.apply = function(message, data) {
-	if (data.nick == "twipkr") {
-		if ( (message.indexOf("ACTION")!=-1) && (message.indexOf("\\u0001")!=-1) ) {
-			message = message.replace(/\\u0001/g, "").replace("ACTION", "");
-				
-			var amount = message.match(/ (\d+,)*\d{3}/g);
-			if  (amount == null) {
-				twipMsg = twipMsg.replace("{bits}", "").
-					replace(/\{0:([^\}]*)}/g, "$1").replace(/\{!0:([^\}]*)}/g, "");
-			}
-			else {
-				amount = amount[0].split(" ")[1].replace(/[^\d]/g, "");
-				twipMsg = twipMsg.replace("{bits}", amount).replace(/((\d)\s+)?비트/, "$2원").
-					replace(/\{!0:([^\}]*)}/g, "$1").replace(/\{0:([^\}]*)}/g, "");
-			}
-			
-			message = '<div class="chat_cheer_box">' + twipMsg + "</div>" + message;
-		}
-	}
-	
-	return message;
-}
+  if (data.nick == "twipkr") {
+    if ( (data.message.indexOf("ACTION")!=-1) && (data.message.indexOf("\\u0001")!=-1) ) {
+
+      var amount = message.match(/ (\d+,)*\d{3}/g);
+      if  (amount == null) {
+        twipMsg = twipMsg.replace("{bits}", "").
+          replace(/\{0:([^\}]*)}/g, "$1").replace(/\{!0:([^\}]*)}/g, "");
+      }
+      else {
+        amount = amount[0].split(" ")[1].replace(/[^\d]/g, "");
+        twipMsg = twipMsg.replace("{bits}", amount).replace(/((\d)\s+)?비트/, "$2원").
+          replace(/\{!0:([^\}]*)}/g, "$1").replace(/\{0:([^\}]*)}/g, "");
+      }
+
+      message = '<div class="chat_cheer_box">' + twipMsg + "</div>" + message;
+    }
+  }
+
+  return message;
+};
